ab8baef789b58ff2f0aa62dfe526fd69a2993af8
javi <daklon@localhost.localdomain>
2015-11-17 14:51:30 +0000

    esqueleto del Sketch

---
+++ Sketch_exterior/Sketch_exterior.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  int pin[4] = {1,2,3,4};
+  
+
+}
+
+void firmar(int pin[4]){
+
+  
+}
+
+void cifrar(int pin[4]){
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
